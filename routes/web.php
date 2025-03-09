8f0210ee342d94d9509e6a3ae73ebbf82abeaa91
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2025-03-09 00:17:01 +0000

    Department Model,Migration and Controller added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +17,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::resource('departments', DepartmentController::class);
